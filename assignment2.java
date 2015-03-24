96aed205cae27051b36b03882219314cd958d090
tringuyen100 <tri_nguyenhien@yahoo.com>
2015-03-24 15:47:12 -0400

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -1,59 +1,42 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
-public class assignment2 {
+public class enumseason {
+	public enum Season{ January, February, March, April, May, June, July, August, September, October, November, December}
 	public static void main(String[] args) {
-		String numb1, numb2, numb3, numb4,numb5, max;
-		int med, middle;
-		Double dblnumb1, dblnumb2, dblnumb3, dblnumb4, dblnumb5, dblsum, dblavr, dblmax, dblmin, dblmed, dblmed2;
-		int int1, int2, int3, int4, int5;
+		Season[] choices = {Season.January, Season.February, Season.March, Season.April, Season.May, Season.June, Season.July, Season.August
+				, Season.September, Season.October, Season.November, Season.December};
+		boolean continueLoop = true;
 		
 		
-		System.out.println("Welcome!");
-		System.out.println("Please input five numbers "); //promt user to input number
-		Scanner keyboard = new Scanner(System.in);
-		numb1 = keyboard.next();//input1
-		numb2 = keyboard.next();//input2
-		numb3 = keyboard.next();//input3
-		numb4 = keyboard.next();//input4
-		numb5 = keyboard.next();//input5
+			
+		Season select = (Season)JOptionPane.showInputDialog(null, "Select your account type.", "Account Type", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
 		
+			while (select!=null)	{	
+	 switch(select) {
+	 case January: case February: case December:
+		 JOptionPane.showMessageDialog(null, "Do you want to build a snow man?");
+		 select = (Season)JOptionPane.showInputDialog(null, "Select your account type.", "Account Type", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		 break;
+	 case March: case April: case May:
+		 JOptionPane.showMessageDialog(null, "Happy Spring days!");
+		 select = (Season)JOptionPane.showInputDialog(null, "Select your account type.", "Account Type", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		 break;
+	 case June: case July: case August:
+		 JOptionPane.showMessageDialog(null, "It's a summer time");
+		 select = (Season)JOptionPane.showInputDialog(null, "Select your account type.", "Account Type", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		 break;
+	 case November: case September: case October:
+		 JOptionPane.showMessageDialog(null, "Welcome to the foliage season!");
+		 select = (Season)JOptionPane.showInputDialog(null, "Select your account type.", "Account Type", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+		 break;
+	default:
+		JOptionPane.showMessageDialog(null, "Incorrect choice!");
 		
-		dblnumb1 = Double.parseDouble(numb1); //double value
-		int1 = dblnumb1.intValue(); //change from double to interger for sorting
-		dblnumb2 = Double.parseDouble(numb2);
-		int2 = dblnumb2.intValue();
-		dblnumb3 = Double.parseDouble(numb3);
-		int3 = dblnumb3.intValue();
-		dblnumb4 = Double.parseDouble(numb4);
-		int4 = dblnumb4.intValue();
-		dblnumb5 = Double.parseDouble(numb5);
-		int5 = dblnumb5.intValue(); //
-		
-		dblsum = dblnumb1+dblnumb2+dblnumb3+dblnumb4+dblnumb5; //find the sum
-		System.out.println("The sum of the five numbers is " + dblsum); //display the sum
-		
-		dblavr = (dblnumb1+dblnumb2+dblnumb3+dblnumb4+dblnumb5)/5; //find the average
-		System.out.println("The average of the five number is " + dblavr); // display the avarage
-		
-		dblmax = Math.max(Math.max(dblnumb3, Math.max(dblnumb1, dblnumb4)), Math.max(dblnumb2, dblnumb5)); //find the max
-		System.out.println("The maximum of the five number is " + dblmax); //display the max
-		
-		dblmin = Math.min(Math.min(dblnumb1, Math.min(dblnumb2, dblnumb3)), Math.min(dblnumb3, dblnumb5)); //find the min
-		System.out.println("The minimum of the five number is " + dblmin); //Display the min
-		
-		int set[] = {int1, int2, int3, int4, int5};//setup the set
-		Arrays.sort(set); //reorder it
-		middle = set.length/2;//find the middle number
-		med = set[middle + 0];//find the median
-		System.out.println("The median of the five number is " + med); //diplay the median
-		
-		
-	
-	
-	}
+		} //} else continueLoop= false;
+		} 
+	}}
 
-}
+
 
 
